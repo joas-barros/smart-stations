73f73cecd519bf689167af65773052e1db8a80bd
joas-barros <joas.barros14@hotmail.com>
2025-12-27 10:38:25 -0300

    fix: remote remote interface implementation from IDataCenterService interface

--- src/main/java/datacenter/src/service/IDataCenterService.java
+++ src/main/java/datacenter/src/service/IDataCenterService.java
@@ -2,18 +2,15 @@ package datacenter.src.service;
 
 import device.src.model.IntegrityPacket;
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
+public interface IDataCenterService {
+    IntegrityPacket getAirQualityReport();
+    IntegrityPacket getHealthAlerts();
+    IntegrityPacket getNoisePollutionReport();
+    IntegrityPacket generateThermalComfortReport();
+    IntegrityPacket generateTemperatureRanking();
 
-public interface IDataCenterService extends Remote {
-    IntegrityPacket getAirQualityReport() throws RemoteException;
-    IntegrityPacket getHealthAlerts() throws RemoteException;
-    IntegrityPacket getNoisePollutionReport() throws RemoteException;
-    IntegrityPacket generateThermalComfortReport() throws RemoteException;
-    IntegrityPacket generateTemperatureRanking() throws RemoteException;
-
-    void registerWithAuthServer() throws  RemoteException;
-    void connectToDatabase() throws RemoteException;
-    void startRMIClientService() throws RemoteException;
-    void startEdgeListener() throws RemoteException;
+    void registerWithAuthServer();
+    void connectToDatabase();
+    void startRMIClientService();
+    void startEdgeListener();
 }
